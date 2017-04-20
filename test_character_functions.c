a8a5d8b085997171543f4984d20f66aef4bbf6c2
Steven TOUPIN <stoupin@e2r11p9.42.fr>
2017-04-20 11:37:37 +0200

    Removed references to bonus character functions

--- test_character_functions.c
+++ test_character_functions.c
@@ -8,18 +8,18 @@ void test_character_functions(void)
 	int j;
 	int res;
 	int res_ft;
-	int (*ft_f[])(int) = {&ft_isupper, &ft_islower, &ft_isalpha, &ft_isdigit,
+	int (*ft_f[])(int) = {&ft_isalpha, &ft_isdigit,
 		&ft_isalnum, &ft_isascii, &ft_isprint, &ft_toupper, &ft_tolower};
-	int (*f[])(int) = {&isupper, &islower, &isalpha, &isdigit,
+	int (*f[])(int) = {&isalpha, &isdigit,
 		&isalnum, &isascii, &isprint, &toupper, &tolower};
-	char *fnames[] = {"isupper", "islower", "isalpha", "isdigit",
+	char *fnames[] = {"isalpha", "isdigit",
 		"isalnum", "isascii", "isprint", "toupper", "tolower"};
 	int	all_good;
 
 	all_good = 1;
 	for (i = -1000; i < 1000; i++)
 	{
-		for (j = 0; j < 9; j++)
+		for (j = 0; j < 7; j++)
 		{
 			res = (*f[j])(i);
 			res_ft = (*ft_f[j])(i);
